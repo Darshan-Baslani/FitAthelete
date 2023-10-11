3bfc5a9721b52a47b37d1da293a2fabc84f09158
Darshan-Baslani <darshan.baslani4@gmail.com>
2023-10-11 19:22:56 +0530

    .

---
+++ website/auth.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    render_template('login.html')
+
+
+@auth.route('/signup')
+def signup():
+    render_template('signup.html')
