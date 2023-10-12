d88d6612958834b72556b0e691874d284734c03e
Darshan-Baslani <darshan.baslani4@gmail.com>
2023-10-12 08:30:07 +0530

    .

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,61 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import check_password_hash, generate_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login.html', methods=['GET', 'POST'])
 def login():
-    render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        login_password = request.form.get('login_password')
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, login_password):
+                flash('Logged in successfully!', category='success')
+                
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    
+    return render_template('login.html', boolean=True)
 
 
-@auth.route('/signup')
+@auth.route('/signup.html', methods=['GET', 'POST'])
 def signup():
-    render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        signup_password1 = request.form.get('signup_password1')
+        signup_password2 = request.form.get('signup_password2')
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if email in user.email:
+            flash('Email already Exists', category='error')
+        elif '.' not in email:
+            flash('Enter valid Email', category='error')
+        elif len(email) < 5:
+            flash('Enter valid Email', category='error')
+        elif len(signup_password1) < 7:
+            flash('Password should be greater than 7 character', category='error')
+        elif signup_password1 != signup_password2:
+            flash('Password Does Not Match', category='error')
+        elif len(signup_password1) > 149:
+            flash('Password is too long', category='error')
+        elif len(email) > 149:
+            flash('Email is too long', category='error')
+        else:
+            new_user = User(email=email, password=generate_password_hash(
+                signup_password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            
+            flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('signup.html')
--- website/views.py
+++ website/views.py
@@ -5,3 +5,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('index.html')
+
+
+@views.route('/index.html')
+def home2():
+    return render_template('index.html')
